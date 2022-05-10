bc1a13fb5987365e7cff8c858fa0d42529da7f4e
delaquash <olaide1191@gmail.com>
2022-05-10 11:30:22 +0100

    fixed rendering in property component

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,11 @@
+
+import { ChakraProvider } from '@chakra-ui/react'
+import Head from 'next/head'
+import Router from 'next/router'
+import nprogress from 'nprogress'
 import '../styles/globals.css'
+
+import Layout from '../components/Layout';
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
