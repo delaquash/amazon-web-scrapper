a04270a72cb2a3fa45240e31271fdbd970547fce
delaquash <olaide1191@gmail.com>
2022-05-10 16:23:27 +0100

    implemented ChakraProvider in root folder

--- pages/_app.js
+++ pages/_app.js
@@ -5,10 +5,22 @@ import Router from 'next/router'
 import nprogress from 'nprogress'
 import '../styles/globals.css'
 
-import Layout from '../components/Layout';
+import Layout from '../components/Layout.js'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+// function MyApp({ Component, pageProps }) {
+//   return <Component {...pageProps} />
+// }
+
+function MyApp({ Component, pageProps}) {
+  return (
+      <>
+        <ChakraProvider>
+        <Layout>
+          <Component {...pageProps}/>
+        </Layout>
+      </ChakraProvider>
+      </>
+  )
 }
 
 export default MyApp
