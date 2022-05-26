f87b0cff6c8c216a9b0a2bf42ad5e1286bf11455
delaquash <olaide1191@gmail.com>
2022-05-26 16:48:15 +0100

    Fix router object in eventLogistics

--- pages/event/[eventId].js
+++ pages/event/[eventId].js
@@ -1,8 +1,37 @@
+import { useRouter } from "next/router";
+import { Fragment } from "react";
+import { getEventById } from '../../dummy-data';
+import EventSummary from "../../components/event-detail/event-summary";
+import EventLogistics from "../../components/event-detail/event-logistics";
+import EventContent from "../../components/event-detail/event-content";
 
-const random =()=> {
+const EventDetailPage =()=> {
+
+  /* A hook that gives us access to the router object. */
+  const router = useRouter();
+
+  /* Getting the id from the url. */
+  const eventId = router.query.eventId;
+
+ /* A function that is getting the event by the id. */
+ const event= getEventById(eventId)
+ if(!event) {
+   return <p>No event found</p>
+ }
   return (
-    <div>This is the id page</div>
+    <Fragment>
+      <EventSummary title={event.title}/>
+      <EventLogistics 
+        date={event.date}
+        address={event.location}
+        image={event.image}
+        imageAlt={event.title}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </Fragment>
   )
 }
 
-export default random;
+export default EventDetailPage;
